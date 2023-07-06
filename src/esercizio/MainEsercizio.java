19de6d582467b47aaaf2b55021a09ef95b18f2f9
lucawar <lucaguerra92@yahoo.it>
2023-07-06 16:46:12 +0200

    commit3

--- src/esercizio/MainEsercizio.java
+++ src/esercizio/MainEsercizio.java
@@ -10,13 +10,19 @@ public class MainEsercizio {
 
 	public static void main(String[] args) {
 
-//-------------------------------------------------------------ESERCIZIO 1-----------------------------------------------------------------------
 		List<Product> products = new ArrayList<>();
 		Random rndm = new Random();
 		products.add(new Product(rndm.nextLong(), "HARRY POTTER", "Books", 150));
 		products.add(new Product(rndm.nextLong(), "THE WITCHER", "Books", 120));
 		products.add(new Product(rndm.nextLong(), "IL CODICE DA VINCI", "Books", 50));
+		products.add(new Product(rndm.nextLong(), "BIBERON", "Baby", 10));
+		products.add(new Product(rndm.nextLong(), "PANNOLINI", "Baby", 20));
+		products.add(new Product(rndm.nextLong(), "OMOGENIZZATI", "Baby", 15));
+		products.add(new Product(rndm.nextLong(), "COMPUTER", "Boys", 700));
+		products.add(new Product(rndm.nextLong(), "TELEVISORE", "Boys", 800));
+		products.add(new Product(rndm.nextLong(), "CELLULARE", "Boys", 500));
 
+//-------------------------------------------------------------ESERCIZIO 1-----------------------------------------------------------------------
 		List<Product> filtraProdotti = products.stream().filter(b -> b.getCategory().equals("Books"))
 				.filter(b -> b.getPrice() > 100).collect(Collectors.toList());
 
@@ -34,16 +40,12 @@ public class MainEsercizio {
 		System.out.println("");
 		System.out.println("ESERCIZIO 2");
 		System.out.println("");
-		List<Product> babyProducts = new ArrayList<>();
-		babyProducts.add(new Product(rndm.nextLong(), "BIBERON", "Baby", 10));
-		babyProducts.add(new Product(rndm.nextLong(), "PANNOLINI", "Baby", 20));
-		babyProducts.add(new Product(rndm.nextLong(), "OMOGENIZZATI", "Baby", 15));
 
 		Customer cliente = new Customer(rndm.nextLong(), "Luca", 1);
 
 		List<Order> ordini = new ArrayList<>();
 		ordini.add(new Order(rndm.nextLong(), "SPEDITO", LocalDate.now(), LocalDate.now().minusDays(4), "Baby",
-				babyProducts, cliente));
+				products, cliente));
 
 		List<Order> filtraOrdini = ordini.stream()
 				.filter(o -> o.getProducts().stream().anyMatch(p -> p.getCategory().equals("Baby")))
@@ -63,12 +65,8 @@ public class MainEsercizio {
 		System.out.println("");
 		System.out.println("ESERCIZIO 3");
 		System.out.println("");
-		List<Product> boysProducts = new ArrayList<>();
-		boysProducts.add(new Product(rndm.nextLong(), "COMPUTER", "Boys", 700));
-		boysProducts.add(new Product(rndm.nextLong(), "TELEVISORE", "Boys", 800));
-		boysProducts.add(new Product(rndm.nextLong(), "CELLULARE", "Boys", 500));
 
-		List<Product> filtraProdottiBoys = boysProducts.stream().filter(b -> b.getCategory().equals("Boys")).map(b -> {
+		List<Product> filtraProdottiBoys = products.stream().filter(b -> b.getCategory().equals("Boys")).map(b -> {
 			double sconto = b.getPrice() * 0.9;
 			b.setPrice(sconto);
 			return b;
