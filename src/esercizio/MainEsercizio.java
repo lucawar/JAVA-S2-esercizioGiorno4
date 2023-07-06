663588a6e7d8e3920cc16c166152c6f3ed1e11ba
lucawar <lucaguerra92@yahoo.it>
2023-07-06 17:54:06 +0200

    commit4

--- src/esercizio/MainEsercizio.java
+++ src/esercizio/MainEsercizio.java
@@ -22,6 +22,18 @@ public class MainEsercizio {
 		products.add(new Product(rndm.nextLong(), "TELEVISORE", "Boys", 800));
 		products.add(new Product(rndm.nextLong(), "CELLULARE", "Boys", 500));
 
+		Customer cliente = new Customer(rndm.nextLong(), "Luca", 1);
+		Customer cliente2 = new Customer(rndm.nextLong(), "Emma", 2);
+		Customer cliente3 = new Customer(rndm.nextLong(), "Lillo", 3);
+
+		List<Order> ordini = new ArrayList<>();
+		ordini.add(new Order(rndm.nextLong(), "SPEDITO", LocalDate.now(), LocalDate.now().minusDays(4), "Baby",
+				products, cliente));
+		ordini.add(new Order(rndm.nextLong(), "DA SPEDIRE", LocalDate.now(), LocalDate.now().plusDays(3), "Books",
+				products, cliente2));
+		ordini.add(new Order(rndm.nextLong(), "DA SPEDIRE", LocalDate.now(), LocalDate.now().plusDays(5), "Baby",
+				products, cliente3));
+
 //-------------------------------------------------------------ESERCIZIO 1-----------------------------------------------------------------------
 		List<Product> filtraProdotti = products.stream().filter(b -> b.getCategory().equals("Books"))
 				.filter(b -> b.getPrice() > 100).collect(Collectors.toList());
@@ -30,8 +42,9 @@ public class MainEsercizio {
 		System.out.println("");
 
 		for (Product prodotti : filtraProdotti) {
-			System.out.println("Libro" + " : " + prodotti.getId() + " : " + prodotti.getName() + " : "
-					+ " il suo prezzo è: " + prodotti.getPrice());
+			System.out.println("NUMERO ID" + prodotti.getId());
+			System.out.println("TITOLO LIBRO: " + prodotti.getName());
+			System.out.println("PREZZO: " + prodotti.getPrice());
 
 			System.out.println("-------------------------------------------------------------------------------");
 		}
@@ -41,17 +54,13 @@ public class MainEsercizio {
 		System.out.println("ESERCIZIO 2");
 		System.out.println("");
 
-		Customer cliente = new Customer(rndm.nextLong(), "Luca", 1);
-
-		List<Order> ordini = new ArrayList<>();
-		ordini.add(new Order(rndm.nextLong(), "SPEDITO", LocalDate.now(), LocalDate.now().minusDays(4), "Baby",
-				products, cliente));
-
 		List<Order> filtraOrdini = ordini.stream()
 				.filter(o -> o.getProducts().stream().anyMatch(p -> p.getCategory().equals("Baby")))
 				.collect(Collectors.toList());
 
 		for (Order ord : filtraOrdini) {
+			System.out.println("ORDINE EFFETUATO DA UTENTE: " + cliente.getId() + ":  " + cliente.getName()
+					+ "-LIVELLO: " + cliente.getTier());
 			System.out.println("ORDINE ID: " + ord.getId());
 			System.out.println("CATEGORIA: " + ord.getCategory());
 			System.out.println("DATA DI OGGI: " + ord.getOrderDate());
